3d91589f0f2d60c56216af84f3a9076a9b113195
Johann Schopplich <mail@johannschopplich.com>
2021-09-30 11:55:43 +0200

    ESLint: Fix unexpected usage of `this`

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -2,7 +2,7 @@
   <div class="k-block-type-grid" @dblclick="open">
     <k-layout-field
       :value="content.grid"
-      v-bind="this.grid"
+      v-bind="grid"
       :label="content.title"
       @input="update()"
     />
